691821baa9cfafdd94a23dacbfec6402099b7afe
Steve Wight <stevendouglaswight@gmail.com>
2016-10-29 12:52:52 -0700

    MainVC storyboard

---
+++ Linguist/MainVC.swift
@@ -0,0 +1,19 @@
+//
+//  MainVC.swift
+//  Linguist
+//
+//  Created by Steve on 10/29/16.
+//  Copyright © 2016 31Labs. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
---
+++ Linguist/Models/DuoClient.swift
@@ -0,0 +1,15 @@
+//
+//  DuoClient.swift
+//  Linguist
+//
+//  Created by Steve on 10/29/16.
+//  Copyright © 2016 31Labs. All rights reserved.
+//
+
+import UIKit
+
+class DuoClient: NSObject {
+
+    
+    
+}
