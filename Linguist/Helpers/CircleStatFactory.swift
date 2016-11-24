4dd8aa7be8b9e15473515afbfdb10d2998501a67
Steve Wight <stevendouglaswight@gmail.com>
2016-11-23 21:12:52 -0800

    refactoring of circle stat factory

--- Linguist/Helpers/CircleStatFactory.swift
+++ Linguist/Helpers/CircleStatFactory.swift
@@ -17,40 +17,53 @@ class CircleStatFactory: NSObject {
         let doubleLearned = Double(duoLang.numSkillsLearned)
         let skillPercent = doubleLearned/doubleTotal
         let progressPercent = duoLang.duoLevel.levelPercent()
+        let isExtended = duoUser.streakExtendedToday
         
         circleStats.append(CircleStat(
             title: "Fluency 🇲🇽",
             percent: duoLang.fluencyScore,
             desc: "% fluent",
-            subDesc: ["🔥 \(duoLang.streak) day streak of \(duoUser.dailyGoal)xp goal", "\(extendedString(extended: duoUser.streakExtendedToday)) today \(extendedEmoji(extended: duoUser.streakExtendedToday))"]
-            )!)
+            subDesc: [
+                "🔥 \(duoLang.streak) day streak of \(duoUser.dailyGoal)xp goal",
+                "\(extendedString(isExtended)) today \(extendedEmoji(isExtended))"
+            ]
+        )!)
         circleStats.append(CircleStat(
             title: "Skills 💭",
             percent: skillPercent,
             desc: "% skills complete",
-            subDesc: ["😎 \(duoLang.numSkillsLearned) skills learned", "Next skill '\(duoLang.nextSkillTitle)' 🤘"]
-            )!)
+            subDesc: [
+                "😎 \(duoLang.numSkillsLearned) skills learned",
+                "Next skill '\(duoLang.nextSkillTitle)' 🤘"
+            ]
+        )!)
         circleStats.append(CircleStat(
             title: "Level \(duoLang.duoLevel.current) 👍",
             percent: progressPercent,
             desc: "% level progress",
-            subDesc: ["😁 \(duoLang.duoLevel.progress)xp this level", "\(duoLang.duoLevel.left)xp to next level 🤓"]
-            )!)
+            subDesc: [
+                "😁 \(duoLang.duoLevel.progress)xp this level",
+                "\(duoLang.duoLevel.left)xp to next level 🤓"
+            ]
+        )!)
         circleStats.append(CircleStat(
             title: "Strength 💪",
             percent: duoLang.languageStrength,
             desc: "% language strength",
-            subDesc: ["\(duoUser.lingots) lingots 💎", "🎉 \(duoLang.duoLevel.points)xp total"]
-            )!)
+            subDesc: [
+                "\(duoUser.lingots) lingots 💎",
+                "🎉 \(duoLang.duoLevel.points)xp total"
+            ]
+        )!)
         
         return circleStats
     }
     
-    class func extendedString(extended:Bool)->String {
+    class func extendedString(_ extended:Bool)->String {
         return extended ? "Extended" : "Not extended"
     }
     
-    class func extendedEmoji(extended:Bool)->String {
+    class func extendedEmoji(_ extended:Bool)->String {
         return extended ? "😀" : "😅"
     }
     
