e8dc75f39d3d5cab2ad99d06eefdec2ea096affd
Steve Wight <stevendouglaswight@gmail.com>
2016-11-23 21:35:23 -0800

    refactor circleTileView

--- Linguist/Views/CircleTileView.swift
+++ Linguist/Views/CircleTileView.swift
@@ -25,37 +25,12 @@ open class CircleTileView: UIView {
         super.layoutSubviews()
         
         if (backgroundRingLayer == nil) {
-            backgroundRingLayer = CAShapeLayer()
-            layer.addSublayer(backgroundRingLayer)
-            
-            let insets = CGFloat(lineWidth/2.0)
-            let rect = bounds.insetBy(dx: insets, dy: insets)
-            let path = UIBezierPath(ovalIn: rect)
-            
-            backgroundRingLayer.path = path.cgPath
-            backgroundRingLayer.fillColor = nil
-            backgroundRingLayer.lineWidth = CGFloat(lineWidth)
-            backgroundRingLayer.strokeColor = UIColor(white: 0.5, alpha: 0.3).cgColor
+            setUpBackgroundRingLayer()
         }
         backgroundRingLayer.frame = layer.bounds
         
         if (ringLayer == nil) {
-            ringLayer = CAShapeLayer()
-            
-            let innerInsets = CGFloat(lineWidth/2.0)
-            let innerRect = bounds.insetBy(dx: innerInsets, dy: innerInsets)
-            let innerPath = UIBezierPath(ovalIn: innerRect)
-            
-            ringLayer.path = innerPath.cgPath
-            ringLayer.fillColor = nil
-            ringLayer.lineWidth = CGFloat(lineWidth)
-            ringLayer.strokeColor = UIColor.white.cgColor
-            ringLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-            ringLayer.transform = CATransform3DRotate(
-                ringLayer.transform,
-                CGFloat(-M_PI/2.0), 0, 0, 1
-            )
-            layer.addSublayer(ringLayer)
+            setUpRingLayer()
         }
         ringLayer.frame = layer.bounds
         
@@ -65,22 +40,41 @@ open class CircleTileView: UIView {
     func updateLayerProperties() {
         if (ringLayer != nil) {
             ringLayer.strokeEnd = rating
-            
-            var strokeColor = UIColor.lightGray
-            
-            switch rating {
-            case let r where r >= 0.75:
-                strokeColor = UIColor.white
-            case let r where r >= 0.5:
-                strokeColor = UIColor.white
-            case let r where r >= 0.30:
-                strokeColor = UIColor.white
-            default:
-                strokeColor = UIColor.white
-            }
-            
-            ringLayer.strokeColor = strokeColor.cgColor
+            ringLayer.strokeColor = UIColor.white.cgColor
         }
+    }
+    
+    func setUpBackgroundRingLayer() {
+        backgroundRingLayer = CAShapeLayer()
+        layer.addSublayer(backgroundRingLayer)
+        
+        let insets = CGFloat(lineWidth/2.0)
+        let rect = bounds.insetBy(dx: insets, dy: insets)
+        let path = UIBezierPath(ovalIn: rect)
+        
+        backgroundRingLayer.path = path.cgPath
+        backgroundRingLayer.fillColor = nil
+        backgroundRingLayer.lineWidth = CGFloat(lineWidth)
+        backgroundRingLayer.strokeColor = UIColor(white: 0.5, alpha: 0.3).cgColor
+    }
+    
+    func setUpRingLayer() {
+        ringLayer = CAShapeLayer()
+        
+        let innerInsets = CGFloat(lineWidth/2.0)
+        let innerRect = bounds.insetBy(dx: innerInsets, dy: innerInsets)
+        let innerPath = UIBezierPath(ovalIn: innerRect)
+        
+        ringLayer.path = innerPath.cgPath
+        ringLayer.fillColor = nil
+        ringLayer.lineWidth = CGFloat(lineWidth)
+        ringLayer.strokeColor = UIColor.white.cgColor
+        ringLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        ringLayer.transform = CATransform3DRotate(
+            ringLayer.transform,
+            CGFloat(-M_PI/2.0), 0, 0, 1
+        )
+        layer.addSublayer(ringLayer)
     }
     
 }
